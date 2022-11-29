caca62d46663b6caf8e05fd212f2c61bcb4b63d4
Trunov Sergey <sergeytru15@gmail.com>
2022-11-29 21:57:58 +0300

    Обновление модели User

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 
 class User(AbstractUser):
+    username = models.CharField('Никнейм', max_length=150, unique=True)
+    email = models.EmailField('Почта', unique=True)
     role = models.CharField('Роль', max_length=20, default='user')
     bio = models.TextField('О себе', blank=True)
-
+    first_name = models.CharField('Имя', max_length=30, blank=True)
+    last_name = models.CharField('Фамилия', max_length=150, blank=True)
