c0667cf3d65136eab55d5bbee27e4eeb6dcc365e
Trunov Sergey <sergeytru15@gmail.com>
2022-11-29 19:12:47 +0300

    Create new model User

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    role = models.CharField('Роль', max_length=20, default='user')
+    bio = models.TextField('О себе', blank=True)
+
