1a8dd9603687523b5bd2aab8c05a799faa56f8bc
Trunov Sergey <sergeytru15@gmail.com>
2022-12-03 14:17:35 +0300

    Подключены urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,4 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 
+from .views import signup_view, user_token_view
 
-urlpatterns = []
+v1_auth_patterns = [
+    path('signup/', signup_view),
+    path('token/', user_token_view),
+]
+
+urlpatterns = [
+    path('v1/auth/', include(v1_auth_patterns)),
+]
