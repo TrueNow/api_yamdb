041ffc6d660291f3fe8347e02bbab1bf6d7c9ed1
Тимур Валиулин <tim55913@yandex.ru>
2022-12-05 20:56:57 +0300

    modified

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -114,7 +114,7 @@ AUTH_USER_MODEL = 'reviews.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    'DEFAULT_PAGINATION_CLASS': 'api_yamdb.api.pagination.CustomPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
